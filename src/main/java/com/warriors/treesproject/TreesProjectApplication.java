63aef4ce109e4a091f1d79a3495335deb1360487
ChadReacher <asglus7@gmail.com>
2022-07-23 13:44:38 +0300

    Initialized Spring Boot project

---
+++ src/main/java/com/warriors/treesproject/TreesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.warriors.treesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreesProjectApplication.class, args);
+	}
+
+}
